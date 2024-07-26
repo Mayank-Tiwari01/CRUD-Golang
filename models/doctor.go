42adbbc3b3ba88e56bbee160e84112680dacab07
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-07-27 04:29:09 +0530

    Next Steps: Add Address Field to Doctor Model
    Update the Doctor model to include an address field.
    
    Implement Search Functionality
    
    Add endpoints to search doctors and patients by name.
    Extend Update Functionality
    
    Allow updates to the doctor's name and address.
    Allow updates to the patient's name.

--- models/doctor.go
+++ models/doctor.go
@@ -7,11 +7,12 @@ import (
 )
 
 type Doctor struct {
-	ID        string         `json:"id" gorm:"primary_key"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	Name      string         `json:"name"`
-	ContactNo string         `json:"contact_no"`
+	ID        string    `json:"id" gorm:"primary_key"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	ContactNo string    `json:"contact_no"`
+	//this must be updated and need not to be given when adding doc deatils initially.
 	Address   string         `json:"address"`
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
