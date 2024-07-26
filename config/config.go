c832dad2e13b959d77e01ba775adc4c2d7cd7381
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-07-27 04:23:24 +0530

    All the major functionalities done, used postman to test these out too.
    Create a Doctor
    Get Doctor by ID
    Update Doctor Contact Number
    Create a Patient
    Get Patient by ID
    Update Patient Details
    Get Patients by Doctor ID

--- config/config.go
+++ config/config.go
@@ -10,6 +10,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
+	//using default credentials
 	dsn := "root:@tcp(127.0.0.1:3306)/mayankDB?charset=utf8mb4&parseTime=True&loc=Local"
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
